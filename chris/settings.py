abcd1d416b8e40d78bdcf46bbb13285fdfe55810
Daniel DUONG <daniel@staff.42.fr>
2014-11-20 16:05:21 +0100

    spider ok

---
+++ chris/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for chris project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'chris'
+
+SPIDER_MODULES = ['chris.spiders']
+NEWSPIDER_MODULE = 'chris.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'chris (+http://www.yourdomain.com)'
---
+++ chris/spiders/chris_spider.py
@@ -0,0 +1,76 @@
+import scrapy
+
+from chris.items import ChrisItem
+
+import urlparse
+import json
+import os
+
+class IdentifiantsManquants(Exception):
+    pass
+
+if 'CHRIS_LOGIN' not in os.environ or 'CHRIS_PASSWORD' not in os.environ:
+    raise IdentifiantsManquants("Please set env variables CHRIS_LOGIN and CHRIS_PASSWORD")
+
+LOGIN = os.environ['CHRIS_LOGIN']
+PASSWORD = os.environ['CHRIS_PASSWORD']
+
+class FormDataFactory(object):
+    template = 'term=&search_title=0&userProjectId=54534&postFilterKeyword=&postTitleSearch=0&blog_numberOfGuestPosts=0&blog_numberOfSponsoredPosts=0&blog_numberOfGiveaways=0&blog_productReview=0&blog_adNetworks=0&blog_twitter=0&twitter_low=&twitter_hi=&blog_facebook=0&facebook_low=&facebook_hi=&blog_googleplus=0&blog_youtube=0&youtube_low=&youtube_hi=&blog_pinterest=0&pinterest_low=&pinterest_hi=&blog_instagram=0&instagram_low=&instagram_hi=&prLow=2&prHigh=10&domainAuthority=&safeSearch=1&lastPostDate=2014-07-23T00%3A00%3A00Z+TO+2014-11-20T00%3A00%3A00Z&blog_numberOfAuthors=&lang=en&_search=false&nd=1416489577997&rows={rows}&page={{page}}&sidx=&sord=asc&query%5B%5D={keyword}~OR&blog_location_adm1Name=&blog_location_countryName=&blog_location_name=&search=true'
+
+    def __init__(self, rows, keyword):
+        self.rows = rows
+        self.keyword = keyword
+
+    @property
+    def template(self):
+        return self.template.format(rows=self.rows, keyword=self.keyword)
+
+    def generate_by_page(self, page):
+        raw = self.template.format(page=page)
+        return dict(urlparse.parse_qsl(raw))
+
+
+class ChrisSpider(scrapy.Spider):
+    name = 'chris'
+    start_urls = ['http://app.grouphigh.com/auth/login/?nxtPg=%2Fblogs%2Fsearch%2F']
+    factory = FormDataFactory(rows=250, keyword='finance')
+
+    def parse(self, response):
+        return scrapy.FormRequest.from_response(
+        response,
+        formdata={'userName': LOGIN, 'password': PASSWORD, 'nxtPg': '/blogs/search/'},
+        callback=self.search
+    )
+
+    def search_page(self, page=722):
+        return scrapy.FormRequest(
+        url='http://app.grouphigh.com/blogs/david-search-data/',
+        formdata=self.factory.generate_by_page(page=page),
+        callback=self.extract
+    )
+
+    def search(self, response):
+        return self.search_page()
+
+    def extract(self, response):
+        json_data = json.loads(response.body)
+        current_page = int(json_data['page'])
+        total_pages = int(json_data['total'])
+
+        if 'rows' not in json_data:
+            print '--------------------------------'
+            print 'THE END'
+            print '--------------------------------'
+            return
+        for row in rows:
+            emails = row['cell'].get('blog_emails', [])
+            for email in emails:
+                item = ChrisItem()
+                item['email'] = email
+                yield item
+        print '--------------------------------'
+        print 'Current page {}, total page: {}'.format(current_page, total_pages)
+        print '--------------------------------'
+        if current_page < total_pages:
+            yield self.search_page(current_page + 1)
